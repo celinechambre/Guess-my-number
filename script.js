cff3b1d99b3fcab75c47931333885c9eb708450b
Celine Chambre <chambre.celine@gmail.com>
2020-10-28 10:51:02 +0100

    btn

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
+let score = 12;
 let hightscore = 0;
 
 const displayMessage = function(message) {
@@ -44,13 +44,12 @@ document.querySelector(".check").addEventListener("click", function() {
 
 document.querySelector('.again').addEventListener("click", function() {
     secretNumber = Math.trunc(Math.random() * 20) + 1;
-    score = 20;
+    score = 12;
+    displayMessage('Start guessing...');
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
 
-    displayMessage("Start guessing...")
-    document.querySelector(".score").textContent = score;
-    document.querySelector(".number").textContent = "?";
-    document.querySelector(".guess").textContent = "";
-
-    document.querySelector("body").style.backgroundColor = '#222';
-    document.querySelector(".number").style.width = "15rem";
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
 });
