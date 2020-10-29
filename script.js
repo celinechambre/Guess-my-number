887bd1d2cfec294d19b38c11e8200bf1dd4d5034
Celine Chambre <chambre.celine@gmail.com>
2020-10-29 12:38:11 +0100

    Add enter functionality to guess the number

--- script.js
+++ script.js
@@ -3,34 +3,29 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 12;
 let hightscore = 0;
+const check = document.querySelector(".check")
 
 const displayMessage = function(message) {
     document.querySelector(".message").textContent = message;
 };
 
-document.querySelector(".check").addEventListener("click", function() {
-    const guess = Number(document.querySelector(".guess").value);
 
-// No number entered  
+const checkNumber = function() {
+    const guess = Number(document.querySelector(".guess").value);
+    // No number entered  
     if(!guess) {
         displayMessage("No number")
-
-// Good guess 
+    // Good guess 
     } else if (guess === secretNumber) {
         displayMessage("Correct number")
-
         document.querySelector(".number").textContent = secretNumber;
-
         document.querySelector("body").style.backgroundColor = '#60b347';
-
         document.querySelector(".number").style.width = "30rem";
-
         if(score > hightscore) {
             hightscore = score;
             document.querySelector(".highscore").textContent = hightscore;
         }
-
-// Wrong guess
+    // Wrong guess
     } else if (guess !== secretNumber) {
         if(score > 0) {
             displayMessage(guess > secretNumber ? "Too high" : "Too low");
@@ -40,7 +35,19 @@ document.querySelector(".check").addEventListener("click", function() {
             displayMessage("You lost") 
         }
     }
+};
+
+document.querySelector(".check").addEventListener("click", function() {
+    checkNumber()
 });
+
+document.addEventListener('keydown', function(e) {
+    console.log(e.key);
+    if(e.key === 'Enter') {
+        checkNumber();
+    }
+});
+
 
 document.querySelector('.again').addEventListener("click", function() {
     secretNumber = Math.trunc(Math.random() * 20) + 1;
@@ -53,3 +60,4 @@ document.querySelector('.again').addEventListener("click", function() {
     document.querySelector('body').style.backgroundColor = '#222';
     document.querySelector('.number').style.width = '15rem';
 });
+
